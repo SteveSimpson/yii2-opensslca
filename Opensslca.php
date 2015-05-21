da66a950eb0331c72b4da804218711cd1544f2fc
Steve Simpson <steve.simpson.ctr@dren.hpc.mil>
2015-05-21 17:20:07 -0400

    Minor changes for development.

--- Opensslca.php
+++ Opensslca.php
@@ -6,16 +6,30 @@ use Yii;
 use yii\base\Component;
 
 /**
- * This is just an example.
+ * This module should give you all the low level functions run a simple CA.
+ *
  */
 class Opensslca extends Component
 {
-    public function generateCetificateAuthority()
+    public $errorText = '';
+
+    public function generateCetificateAuthority($force = false)
     {
+
+        if (! $force && file_exists($caKeyFile)) {
+            $this->errorText = 'CA Key Exists, force not set';
+            return false;
+        }
+
+        if (! $force && file_exists($caCertFile)) {
+            $this->errorText = 'CA Key Exists, force not set';
+            return false;
+        }
+
         $pkey = $this->generatePrivateKey();
-        openssl_pkey_export_to_file($pkey, $outfilename, $password);
+        openssl_pkey_export_to_file($pkey, $cafilename, $password);
 
-
+        return true;
     }
 
 
@@ -55,5 +69,10 @@ class Opensslca extends Component
         }
 
         return openssl_pkey_new($configargs);
+    }
+
+    public function run()
+    {
+        return "Hello!";
     }
 }
