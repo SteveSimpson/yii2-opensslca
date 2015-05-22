c22fcdd93d1226f344552994a1949712251e8788
Steve Simpson <steve.simpson.ctr@dren.hpc.mil>
2015-05-22 17:13:31 -0400

    Most functionality is working.

--- Opensslca.php
+++ Opensslca.php
@@ -51,33 +51,46 @@ class Opensslca extends Component
     public $serialFile;
 
 
+    public $caKey;
+
+
+    public $caCert;
+
+
     public function generateCetificateAuthority($force = false)
     {
-        if (! is_dir($this->ca_dir)) {
-            if (! mkdir($this->ca_dir,'0700')) {
+        if (! is_dir($this->getCaDir())) {
+            if (! mkdir($this->getCaDir())) {
                 \Yii::error('Unable to create CA Directory','opensslca');
                 return false;
+            } else {
+                mkdir($this->getCaDir() . "/keys");
+                mkdir($this->getCaDir() . "/certs");
             }
         }
 
-        if (! $force && file_exists($caKeyFile)) {
+        if (! $force && file_exists($this->getCaKeyFile())) {
             \Yii::error('CA Key Exists, force not set','opensslca');
             return false;
         }
 
-        if (! $force && file_exists($caCertFile)) {
-            \Yii::error('CA Key Exists, force not set','opensslca');
+        if (! $force && file_exists($this->getCaCertFile())) {
+            \Yii::error('CA Cert Exists, force not set','opensslca');
             return false;
         }
 
-        $pkey = $this->generatePrivateKey();
+        $pkey = $this->generatePrivateKey('1001');
 
-        if (! openssl_pkey_export_to_file($pkey, $caKeyFile, $this->password)) {
+        //print_r($pkey); die();
+
+        if (! openssl_pkey_export_to_file($pkey, $this->getcaKeyFile(), $this->password)) {
             \Yii::error('Unable to write CA Key','opensslca');
             return false;
         }
+        chmod($this->getCaKeyFile(), 0400);
 
-        if (! file_put_contents($this->serialFile, '1001', LOCK_EX)) {
+
+        if (! file_put_contents($this->getSerialFile(), '1001', LOCK_EX)) {
             \Yii::error('Unable to write serial file.','opensslca');
             return false;
         }
@@ -94,8 +107,13 @@ class Opensslca extends Component
         // this is slightly different since this is a self-signed cert; cacert is null
         // use a 10 year CA
         $cacert = openssl_csr_sign($csr, null, $pkey, 3650, $configargs, '1001');
+        openssl_x509_export_to_file($cacert, $this->getCaDir() . "/certs/1001" );
 
         openssl_x509_export_to_file($cacert, $this->caCertFile);
+
+        while (($e = openssl_error_string()) !== false) {
+            \Yii::warning($e,'Opensslca::generateCetificateAuthority');
+        }
 
         return true;
     }
@@ -114,13 +132,20 @@ class Opensslca extends Component
     }
 
 
-    public function signCertificate($csr, $days)
+    public function signCertificate($csr, $serial, $days, $configargs = null)
     {
-        $pkey = openssl_pkey_get_private("file://".$this->caKeyFile,$this->password);
+       if (is_null($configargs)) {
+            $configargs = [
+                "digest_alg" => "sha512",
+                'x509_extensions' => 'v3_req',
+            ];
+        }
 
-        $configargs['config'] = $this->getSslConfig();
+        $cacert = openssl_csr_sign($csr, $this->getCaCert(), $this->getCaKey(), $days, $configargs, intval($serial));
 
-        //openssl_csr_sign($csr, );
+        openssl_x509_export_to_file($cacert, $this->getCaDir() . "/certs/" . $serial);
+
+        return $cacert;
     }
 
 
@@ -128,10 +153,11 @@ class Opensslca extends Component
     {
         if (is_null($configargs)) {
             $configargs = [
-            "digest_alg" => "sha512",
+                "digest_alg" => "sha512",
+                'x509_extensions' => 'v3_req',
             ];
         }
-        $configargs['config'] = $this->getSslConfig();
+        //$configargs['config'] = $this->getSslConfig();
 
         $signDn = $this->dn_base;
 
@@ -139,63 +165,136 @@ class Opensslca extends Component
             $signDn[$key] = $value;
         }
 
-        return openssl_csr_new($dn, $pkey, $configargs);
+        while (($e = openssl_error_string()) !== false) {
+            \Yii::warning($e,'Opensslca::createCertificatSigningRequest');
+        }
+
+        return openssl_csr_new($signDn, $pkey, $configargs);
     }
 
 
-    public function generatePrivateKey($configargsIn = [])
+    public function generatePrivateKey($sn, $configargsIn = [])
     {
-
         $configargs = [
             "digest_alg" => "sha512",
             "private_key_bits" => 2048,
             "private_key_type" => OPENSSL_KEYTYPE_RSA,
-            'config' => $this->getSslConfig(),
         ];
+        //echo $this->getSslConfig();
 
         foreach ($configargsIn as $key=>$value) {
             $configargs[$key] = $value;
         }
 
-        return openssl_pkey_new($configargs);
+        $pkey = openssl_pkey_new($configargs);
+
+        if (! openssl_pkey_export_to_file($pkey, $this->getCaDir() . "/keys/" . $sn, $this->password)) {
+            \Yii::error('Unable to write key '.$sn,'opensslca');
+        }
+        chmod($this->getCaDir() . "/keys/" . $sn, 0400);
+
+        while (($e = openssl_error_string()) !== false) {
+            \Yii::warning($e,'Opensslca::generatePrivateKey');
+        }
+
+        return $pkey;
     }
 
-    public function getca_dir()
+
+    public function privateKeyToString($pkey, $password = null)
+    {
+        $ouput = false;
+        if (!openssl_pkey_export($pkey, $output, $password)) {
+            while (($e = openssl_error_string()) !== false) {
+                \Yii::warning($e,'Opensslca::privateKeyToString');
+            }
+        }
+        return $output;
+    }
+
+    public function certificateToString($cert, $notext = true)
+    {
+        $ouput = false;
+        if (!openssl_x509_export($cert, $output, $notext)) {
+            while (($e = openssl_error_string()) !== false) {
+                \Yii::warning($e,'Opensslca::privateKeyToString');
+            }
+        }
+        return $output;
+    }
+
+
+    public function getCaDir()
     {
         return str_replace('@app', \Yii::$app->basePath, $this->ca_dir);
     }
 
-    public function getcaKeyFile()
+    public function getCaKeyFile()
     {
         if (!isset($this->caKeyFile)) {
-            $this->caKeyFile = $this->ca_dir . "ca_key.pem";
+            $this->caKeyFile = $this->getCaDir() . "/ca_key.pem";
         }
         return $this->caKeyFile;
     }
 
-    public function getcaCertFile()
+    public function getCaCertFile()
     {
         if (!isset($this->caCertFile)) {
-            $this->caCertFile = $this->ca_dir . "ca_cert.pem";
+            $this->caCertFile = $this->getCaDir() . "/ca_cert.pem";
         }
         return $this->caCertFile;
     }
 
-    public function getserialFile()
+    public function getSerialFile()
     {
         if (!isset($this->serialFile)) {
-            $this->serialFile = $this->ca_dir . "ca_serial";
+            $this->serialFile = $this->getCaDir() . "/ca_serial";
         }
         return $this->serialFile;
     }
 
     public function getSslConfig()
     {
+        if (file_exists($this->getCaDir() . "/openssl.cnf")) {
+            return $this->getCaDir() . "/openssl.cnf";
+        }
         return dirname(__FILE__) . "/openssl.cnf";
     }
 
-    public function getSerial()
+    public function getNextSerial()
     {
+        $sn = (int) file_get_contents($this->getSerialFile());
+        $sn++;
+        if (! file_put_contents($this->getSerialFile(), $sn, LOCK_EX)) {
+            \Yii::error('Unable to write serial file.','opensslca');
+            return false;
+        }
+        return $sn;
+    }
 
+    public function getCaKey()
+    {
+        if (! $this->caKey) {
+            $this->caKey = openssl_pkey_get_private("file://".$this->getCaKeyFile(), $this->password);
+        }
+
+        while (($e = openssl_error_string()) !== false) {
+            \Yii::warning($e,'Opensslca::getCaKey');
+        }
+
+        return $this->caKey;
+    }
+
+    public function getCaCert()
+    {
+        if (! $this->caCert) {
+            $this->caCert = openssl_x509_read("file://".$this->getCaCertFile());
+        }
+
+        while (($e = openssl_error_string()) !== false) {
+            \Yii::warning($e,'Opensslca::getCaCert');
+        }
+
+        return $this->caCert;
     }
 }
